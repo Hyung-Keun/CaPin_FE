ec64314d430704bcd1648a38ad281e1bfde113f0
Sehyeon Park <nesmile23@gmail.com>
2022-05-12 21:43:02 +0900

    chore: 환경변수 세팅
    - env-cmd로 환경별 .env 파일 분리
    - 절대경로 버그 수정

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 
-import reportWebVitals from "reportWebVitals";
-
 import store from "@redux/configureStore";
 
 import App from "@shared/App";
+
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
