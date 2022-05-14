deac80f1e29a6c7d92561a4038c29dc9e1cc9a3b
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-05-14 14:51:08 +0900

    Pull from feature/login to feature/profilePage

---
+++ src/elements/Input.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+interface IInput{
+    children?:React.ReactNode;
+    inlineStyles?:string;
+}
+const Input = () => {
+    return(
+
+    )
+};
+export default Input;
