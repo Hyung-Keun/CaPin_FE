9cd2631575fa027f5d83559a8c40d6e865f48149
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-05-14 14:51:40 +0900

    Change in Input.tsx

--- src/elements/Input.tsx
+++ src/elements/Input.tsx
@@ -4,10 +4,16 @@ import styled from "styled-components";
 interface IInput{
     children?:React.ReactNode;
     inlineStyles?:string;
+    placeholder?:string;
+    onChange?:
+
 }
+
 const Input = () => {
     return(
+        <React.Fragment>
 
+        </React.Fragment>
     )
 };
 export default Input;
