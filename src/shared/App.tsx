38bd076a554846f58eac2b546eba5c260d5f36f3
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-05-17 20:20:28 +0900

    Feat: 카카오 지도 api 기능 추가
    index.html-> javascript api key 추가 (추후 경로에 추가 필요).
    KakaoMap.tsx-> 카카오지도에 geolocation  추가에서 본인위치 조회하기기능 구현.
    App.tsx-> import KakaoMap.

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -6,6 +6,7 @@ import OAuth from "@pages/Auth/OAuth";
 import Profile from "@pages/Auth/Profile";
 import NotFound from "@pages/Error/NotFound";
 import Home from "@pages/Home";
+import KakaoMap from "@pages/KakaoMap";
 
 import GlobalStyles from "./GlobalStyles";
 
@@ -20,6 +21,7 @@ const App = () => {
           <Route path="/notfound" element={<NotFound />} />
           <Route path="/oauth" element={<OAuth />} />
           <Route path="/profile" element={<Profile />} />
+          <Route path="/map" element={<KakaoMap />} />
           <Route path="*" element={<Navigate to="/notfound" replace />} />
         </Routes>
       </BrowserRouter>
