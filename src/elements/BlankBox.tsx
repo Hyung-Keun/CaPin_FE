5ab5ce17a6c82d130673c153d13a15e7422624cf
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-05-19 02:29:12 +0900

    Add: 빈칸용 element 추가
    BlankBox.tsx-> 빈칸 및 빈공간 그리드 필요시 사용.

---
+++ src/elements/BlankBox.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import styled from "styled-components";
+
+interface IBlankBox {
+  children?: React.ReactNode;
+  value?: string | number;
+  inlineStyles?: string;
+}
+const BlankBox = ({ children, value, inlineStyles }: IBlankBox) => {
+  return (
+    <React.Fragment>
+      <Blank value={value} inlineStyles={inlineStyles}>
+        {children}
+      </Blank>
+    </React.Fragment>
+  );
+};
+
+const Blank = styled.div<IBlankBox>`
+  ${(props) => (props.inlineStyles ? `${props.inlineStyles};` : "")}
+`;
+export default BlankBox;
