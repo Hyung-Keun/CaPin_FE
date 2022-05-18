bdc97bf04d6f49236e1c3847a79109bfabe64a86
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-05-18 15:58:28 +0900

    Add: element에 Select.tsx추가 및 index에 추가.

--- src/elements/index.ts
+++ src/elements/index.ts
@@ -2,6 +2,7 @@ import Button from "./Button";
 import Grid from "./Grid";
 import Image from "./Image";
 import Input from "./Input";
+import Select from "./Select";
 import Text from "./Text";
 
-export { Button, Grid, Image, Text, Input };
+export { Button, Grid, Image, Text, Input, Select };
