cb6afe1bd83599a4cf81e65c12ce15451c6bfd1d
Sehyeon Park <nesmile23@gmail.com>
2022-05-21 10:08:39 +0900

    feat: 지도관련 데이터 type 추가 및 수정

--- src/@type/init.ts
+++ src/@type/init.ts
@@ -9,3 +9,27 @@ export interface INavItem {
 export interface IInitState {
   navList: INavItem[];
 }
+
+export interface IStudyCard {
+  id: number;
+  name: string;
+  meetArea: string;
+  members: string[];
+  midPoint: string;
+}
+
+export interface ICafeData {
+  id: string;
+  imgSrc: string;
+  name: string;
+  addr: string;
+  hours: string;
+  rate: number;
+  reviewCnt: number;
+  position: IPosition;
+}
+
+export interface IPosition {
+  lat: number;
+  lng: number;
+}
