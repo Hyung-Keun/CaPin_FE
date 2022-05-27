a9e357e8b1a93d584827cd66450c32f619a9e1df
semyung oh <semyung.dev@gmail.com>
2022-05-28 03:35:14 +0900

    feat: 유저의 스터디 그룹 목업 컴포넌트 추가

---
+++ src/pages/MyPage/mock.ts
@@ -0,0 +1,46 @@
+import { ICurrentGroups } from "./types";
+
+function generateUUID() {
+  let d = new Date().getTime();
+  let d2 =
+    (typeof performance !== "undefined" &&
+      performance.now &&
+      performance.now() * 1000) ||
+    0;
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    let r = Math.random() * 16;
+    if (d > 0) {
+      r = (d + r) % 16 | 0;
+      d = Math.floor(d / 16);
+    } else {
+      r = (d2 + r) % 16 | 0;
+      d2 = Math.floor(d2 / 16);
+    }
+    return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+  });
+}
+
+export default function getMockupCurrentGroups(count = 12): ICurrentGroups {
+  const result = [];
+  for (let i = 0; i < count; i++) {
+    const groupId = generateUUID();
+    const groupTitle = "영어 회화 원어민 도전";
+    const description = generateUUID() + generateUUID();
+    const memberCount = Math.floor(Math.random() * 20);
+    const maxMemberCount = memberCount + Math.floor(Math.random() * 10);
+    const roughAddress = "선릉역/강남역/교대역";
+    const firstDay = "2022.01.03";
+    const lastDay = "2022.05.28";
+    result.push({
+      groupId,
+      groupTitle,
+      description,
+      memberCount,
+      maxMemberCount,
+      roughAddress,
+      firstDay,
+      lastDay,
+    });
+  }
+  return result;
+}
