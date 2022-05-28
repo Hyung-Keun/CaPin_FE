d95a07a22631482536a89cb858f4dc92644e8b77
Sehyeon Park <nesmile23@gmail.com>
2022-05-28 23:17:22 +0900

    fix: 스터디 생성 요청 오류 수정
    - 지역값 key 수정
    - content-type 제거
    (rtk query에서 데이터 형태 보고 알아서 세팅해줌.)

--- src/redux/api/studyApi.ts
+++ src/redux/api/studyApi.ts
@@ -15,9 +15,6 @@ export const studyApi = createApi({
       query: (data) => ({
         url: `/api/groups`,
         method: "POST",
-        headers: {
-          "content-type": "multipart/form-data",
-        },
         body: data,
       }),
     }),
