1734d74eb13ea6f0e64aae39cf31ebd186d619e3
semyung oh <semyung.dev@gmail.com>
2022-05-29 04:54:09 +0900

    feat: 유저 스터디 그룹 페이지 상단 뒤로가기 버튼 추가

--- src/pages/MyPage/UserStudyGroups.tsx
+++ src/pages/MyPage/UserStudyGroups.tsx
@@ -4,39 +4,27 @@ import styled from "styled-components";
 
 import StudyGroupCard from "@components/StudyGroupCard";
 import { DefaultStudyGroupListLayout } from "@components/StudyGroupList";
+import TitleWithBackButton from "@components/TitleWithBackButton";
 
 import getMockupCurrentGroups from "./mock";
 import { ICommonProps } from "./types";
 
 import { typography } from "@utils/const";
 
-const Header = styled.header`
-  width: 100%;
-  height: 43px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  margin-bottom: 9px;
-
-  & > h1 {
-    ${typography.st17b};
-  }
-`;
-
-const UserStudyGroups = ({ onBackButtonClick }: ICommonProps) => {
-  const mockups = getMockupCurrentGroups(20);
+const UserStudyGroups = ({ goBack }: ICommonProps) => {
   const navigate = useNavigate();
   const navigateToDetailPage = () => {
     // navigate()
   };
 
+  const mockups = getMockupCurrentGroups(20);
+
   return (
     <>
       {/* TODO: ADD Assets */}
-      <Header onClick={onBackButtonClick}>
-        <h1>내 스터디 그룹</h1>
-      </Header>
+      <TitleWithBackButton title="내 스터디 그룹" onBackButtonClick={goBack} />
+      {/* <h1>내 스터디 그룹</h1> */}
+
       <DefaultStudyGroupListLayout>
         {mockups.map((mockup) => (
           <StudyGroupCard
