25d3adadb0b0041b93de8c468c7f3c32b5164734
Hyung-Keun <hyungkeun.kang@gmail.com>
2022-06-01 01:42:12 +0900

    Chore: onClick 추가.
    BlankBox.tsx: onClick 추가.

--- src/elements/BlankBox.tsx
+++ src/elements/BlankBox.tsx
@@ -2,22 +2,28 @@ import React from "react";
 
 import styled from "styled-components";
 
-interface IBlankBox {
+interface IBlankBox<T> {
   children?: React.ReactNode | React.ReactNode[];
   value?: string | number;
   inlineStyles?: string;
+  onClick?: React.MouseEventHandler<T>;
 }
-const BlankBox = ({ children, value, inlineStyles }: IBlankBox) => {
+const BlankBox = ({
+  children,
+  value,
+  inlineStyles,
+  onClick,
+}: IBlankBox<HTMLButtonElement & HTMLInputElement>) => {
   return (
     <React.Fragment>
-      <Blank value={value} inlineStyles={inlineStyles}>
+      <Blank value={value} inlineStyles={inlineStyles} onClick={onClick}>
         {children}
       </Blank>
     </React.Fragment>
   );
 };
 
-const Blank = styled.div<IBlankBox>`
+const Blank = styled.div<IBlankBox<HTMLButtonElement & HTMLInputElement>>`
   ${(props) => (props.inlineStyles ? `${props.inlineStyles};` : "")}
 `;
 export default BlankBox;
