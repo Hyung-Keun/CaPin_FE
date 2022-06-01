53155820ab210d2b7881b859f64f4c77398dcd05
Sehyeon Park <nesmile23@gmail.com>
2022-06-01 21:32:42 +0900

    chore: loading isSolid prop 추가

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,24 +4,31 @@ import Icon from "./Icon";
 
 import { palette } from "@utils/const";
 
-const Loading = () => {
+interface ILoading {
+  isSolid?: boolean;
+}
+
+const Loading = ({ isSolid = false }: ILoading) => {
   return (
-    <Container>
+    <Container isSolid={isSolid}>
       <CircleArea>
         <Icon type="LogoOrange" />
       </CircleArea>
+      {isSolid && <div>잠시만 기다려 주세요.</div>}
     </Container>
   );
 };
 
-const Container = styled.section`
+const Container = styled.section<ILoading>`
   position: fixed;
   top: 0;
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: ${palette.grey050};
+  background-color: ${({ isSolid }) =>
+    isSolid ? palette.white : "rgba(0, 0, 0, 0.5)"};
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   z-index: 999;
