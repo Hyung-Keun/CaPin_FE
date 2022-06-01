c7bb2d6690c749c8a61f893b19f1f4e4e2b39a2e
Sehyeon Park <nesmile23@gmail.com>
2022-06-02 02:53:10 +0900

    feat: 특정 스터디 페이지 수정
    - 기능추가 및 스타일 수정

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -1,32 +1,140 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
-import gamst from "../assets/images/gamst.jpeg";
+import styled from "styled-components";
 
-import { Text, Image, BlankBox } from "@elements";
+import { MemberAuthority, SpecificGroupResponse } from "@type/group";
 
-const People = () => {
+import {
+  useApproveMemberMutation,
+  useDenyMemberMutation,
+  useExitStudyMutation,
+} from "@redux/api/studyApi";
+import { useGetUserQuery } from "@redux/api/userApi";
+
+import Icon from "./Icon";
+
+import { Text, Image } from "@elements";
+import useUpDownModal from "@hooks/useUpDownModal";
+import { palette, typography } from "@utils/const";
+import { convertPixelToRem } from "@utils/func";
+
+const People = ({
+  groupId,
+  memberList,
+}: {
+  groupId: SpecificGroupResponse["groupId"];
+  memberList: SpecificGroupResponse["memberList"];
+}) => {
+  const navigate = useNavigate();
+  const memberIdRef = useRef(-1);
+  const { data: userData } = useGetUserQuery(null);
+  const [approve] = useApproveMemberMutation();
+  const [deny] = useDenyMemberMutation();
+  const [exit] = useExitStudyMutation();
+  const { UpDownModal, open } = useUpDownModal();
+  const userAuthority = memberList?.find(
+    (member) => member.memberId === userData?.memberId,
+  )?.authority;
+
+  const modalButtonData = [
+    {
+      text: "승인",
+      onClick: () => approve({ groupId, memberId: memberIdRef.current }),
+    },
+    {
+      text: "거절",
+      onClick: () => deny({ groupId, memberId: memberIdRef.current }),
+    },
+  ];
+
+  const exitStudy = () => {
+    if (groupId) {
+      exit(groupId);
+      navigate("/grouplist");
+    }
+  };
+
   return (
-    <React.Fragment>
-      <Text inlineStyles="margin:0px 0px 28px 20px">스터디원목록</Text>
-      <BlankBox inlineStyles="display: flex; flex-direction: column">
-        <BlankBox inlineStyles="display:flex">
-          <Image
-            inlineStyles="margin: 0px 10px 8px 20px"
-            shape="circle"
-            size="30px"
-            src={gamst}
-          />
-          <Text inlineStyles="margin: 5px 254px 27px 0px">회원</Text>
-        </BlankBox>
-        <Text
-          inlineStyles="text-decoration-line: underline;
-        margin: 65px 0px 0px 20px;
-        "
-        >
-          스터디 나가기
-        </Text>
-      </BlankBox>
-    </React.Fragment>
+    <>
+      <Title>스터디원목록</Title>
+      <StyleUl>
+        {memberList?.map((member) => (
+          <li key={member.memberId}>
+            <InfoWrap>
+              <Image
+                shape="circle"
+                size="30px"
+                inlineStyles={`margin-right: ${convertPixelToRem(10)}`}
+                src={member.imageUrl!}
+              />
+              <Text>{member.username}</Text>
+            </InfoWrap>
+            {userAuthority === MemberAuthority.OWNER &&
+              member.authority === MemberAuthority.WAIT && (
+                <OptionButton
+                  onClick={() => {
+                    memberIdRef.current = member.memberId;
+                    open();
+                  }}
+                >
+                  승인
+                  <Icon type="ArrowDownWhite" />
+                </OptionButton>
+              )}
+          </li>
+        ))}
+      </StyleUl>
+      <ExitButton onClick={exitStudy}>스터디 나가기</ExitButton>
+      <UpDownModal buttonData={modalButtonData} />
+    </>
   );
 };
+
+const Title = styled.h2`
+  ${typography.b14r}
+  margin-bottom: ${convertPixelToRem(20)};
+`;
+
+const StyleUl = styled.ul`
+  & > li {
+    ${typography.b14r}
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    &:not(:last-child) {
+      margin-bottom: ${convertPixelToRem(8)};
+    }
+  }
+`;
+
+const InfoWrap = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const OptionButton = styled.button`
+  ${typography.b13sb}
+  background-color: ${palette.orange600};
+  color: ${palette.white};
+  border: none;
+  border-radius: ${convertPixelToRem(4)};
+  padding: ${convertPixelToRem(6)} ${convertPixelToRem(12)};
+  text-align: center;
+  & > svg {
+    margin-left: ${convertPixelToRem(8)};
+  }
+`;
+
+const ExitButton = styled.button`
+  font-size: 14px;
+  padding: 0;
+  text-decoration-line: underline;
+  margin-top: ${convertPixelToRem(24)};
+  color: ${palette.grey600};
+  cursor: pointer;
+`;
+
 export default People;
--- src/pages/SpecificStudy.tsx
+++ src/pages/SpecificStudy.tsx
@@ -1,69 +1,226 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
+import styled from "styled-components";
+
+import { MemberAuthority } from "@type/group";
+
+import Icon from "@components/Icon";
+import Loading from "@components/Loading";
 import People from "@components/People";
 
-import defaultImg from "../assets/defaultImg.svg";
+import {
+  useApplyStudyMutation,
+  useLazyGetSpecificStudyQuery,
+} from "@redux/api/studyApi";
+import { useGetUserQuery } from "@redux/api/userApi";
 
-import { BlankBox, Text, Button } from "@elements";
+import groupDefault from "@assets/images/group_default.jpg";
+import useUpDownModal from "@hooks/useUpDownModal";
+import { palette, typography } from "@utils/const";
+import { convertPixelToRem } from "@utils/func";
 
+/**
+ * 앞으로 구현해야할 기능들
+ * 1. 관리자 > 모집글 수정/종료, 스터디삭제 기능
+ * 2. 공유하기 기능
+ *
+ */
 const SpecificStudy = () => {
+  const navigate = useNavigate();
+  const params = useParams();
+  const { id: groupId } = params;
+  const [getSpecificStudyData, { data }] = useLazyGetSpecificStudyQuery();
+  const { data: userData, refetch } = useGetUserQuery(null);
+  const [apply, { isSuccess: isSuccessApply, isLoading: isLoadingApply }] =
+    useApplyStudyMutation();
+  const { UpDownModal, open: openUpDownModal } = useUpDownModal();
+
+  useLayoutEffect(() => {
+    if (groupId) getSpecificStudyData(groupId);
+  }, [groupId]);
+
+  const isMember =
+    data?.memberList.findIndex(
+      (member) => member.memberId === userData?.memberId,
+    ) !== -1;
+
+  const isOwner =
+    data?.memberList.findIndex(
+      (member) =>
+        member.memberId === userData?.memberId &&
+        member.authority === MemberAuthority.OWNER,
+    ) !== -1;
+
+  const goPlaceSearch = () => {
+    if (groupId) navigate(`/placesearch/${groupId}`);
+  };
+
+  const applyGroup = () => {
+    if (groupId) apply(groupId);
+  };
+
+  useEffect(() => {
+    if (isSuccessApply) refetch();
+  }, [isSuccessApply]);
+
+  /**
+   * 모집글 수정/종료, 스터디삭제 기능 구현 필요.
+   */
+  const buttonData = [
+    {
+      text: "모집글 수정",
+      onClick: () => {},
+    },
+    {
+      text: "모집 종료",
+      onClick: () => {},
+    },
+    {
+      text: "스터디 삭제",
+      onClick: () => {},
+    },
+  ];
+
   return (
-    <React.Fragment>
-      <img alt="test" width={375} height={210} src={defaultImg} />
-      <BlankBox
-        inlineStyles="position: relative;
-width: 375px;
-height: auto;
-background: #FFFFFF;
-box-shadow: 0px -2px 20px rgba(0, 0, 0, 0.1);
-border-radius: 14px 14px 0px 0px;"
-      >
-        <Text
-          inlineStyles="margin: 40px 171px 10px 20px;font-family: 'Noto Sans KR';
-font-style: normal;
-font-weight: 700;
-font-size: 22px;
-line-height: 32px;
-letter-spacing: -0.04em;
-color: #111111; "
-        >
-          브랜드 마케팅 스터디
-        </Text>
-        <Text inlineStyles="margin: 0px 20px 30px 20px">
-          안녕하세요. 브랜드 마케팅 스터디 모임장 모임장입니다.안녕하세요.
-          브랜드 마케팅 스터디 모임장 모임장입니다. 안녕하세요. 브랜드 마케팅
-          브랜드 마케팅 스터디 모임장 모임장입니다. 안녕하세요. 브랜드 마케팅
-          브랜드 마케팅 스터디 모임장 모임장입니다. 안녕하세요. 브랜드 마케팅{" "}
-        </Text>
-        <BlankBox inlineStyles="display: flex">
-          <Text inlineStyles="margin: 0px 20px 10px 20px">지역</Text>
-          <Text inlineStyles="margin: 0px 20px 10px 20px">
-            잠실, 석촌, 장지
-          </Text>
-        </BlankBox>
-        <BlankBox inlineStyles="display: flex">
-          <Text inlineStyles="margin: 0px 20px 10px 20px">기간</Text>
-          <Text inlineStyles="margin: 0px 20px 10px 20px">
-            2022.04.02~2022.09.23
-          </Text>
-        </BlankBox>
-        <BlankBox inlineStyles="display: flex">
-          <Text inlineStyles="margin: 0px 20px 10px 20px">인원</Text>
-          <Text inlineStyles="margin: 0px 20px 10px 20px">5/10명</Text>
-        </BlankBox>
-        <Button
-          inlineStyles="    margin: 238px 20px 41px 20px;
-    width: 334px;
-    height: 48px;
-    border-radius: 10px;
-    background: #4F4F4F
-    "
-        >
-          <Text inlineStyles="color: white">참여하기</Text>
-        </Button>
-        <People />
-      </BlankBox>
-    </React.Fragment>
+    <>
+      {isLoadingApply && <Loading />}
+      <Container headerImage={data?.imageUrl || groupDefault}>
+        <header>
+          <button onClick={() => navigate(-1)}>
+            <Icon type="CircleArrowLeft" />
+          </button>
+          <ButtonWrap>
+            {isOwner && (
+              <button onClick={openUpDownModal}>
+                <Icon type="CircleDotsVertical" />
+              </button>
+            )}
+            <button>
+              <Icon type="CircleShare" />
+            </button>
+          </ButtonWrap>
+        </header>
+        <ContentArea>
+          <h1>{data?.groupTitle}</h1>
+          <p>{data?.description}</p>
+          <GroupInfoArea>
+            <ul>
+              <li>
+                <h2>지역</h2>
+                <div>{data?.roughAddress}</div>
+              </li>
+              <li>
+                <h2>기간</h2>
+                <div>
+                  {data?.firstDay} ~ {data?.lastDay}
+                </div>
+              </li>
+              <li>
+                <h2>인원</h2>
+                <div>
+                  {data?.memberCount} / {data?.maxMemberCount} 명
+                </div>
+              </li>
+            </ul>
+          </GroupInfoArea>
+          <button onClick={isMember ? goPlaceSearch : applyGroup}>
+            {isMember ? "중간장소 추천받기" : "참여하기"}
+          </button>
+        </ContentArea>
+        <Divider />
+        <PeopleArea>
+          <People groupId={data?.groupId!} memberList={data?.memberList!} />
+        </PeopleArea>
+      </Container>
+      <UpDownModal buttonData={buttonData} />
+    </>
   );
 };
+
+const Container = styled.section<{ headerImage: string }>`
+  width: 100%;
+  & > header {
+    width: 100%;
+    height: ${convertPixelToRem(210)};
+    background-image: url(${({ headerImage }) => headerImage});
+    background-size: cover;
+    background-position: center;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 0 ${convertPixelToRem(20)};
+    padding-top: ${convertPixelToRem(56)};
+  }
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ContentArea = styled.main`
+  position: relative;
+  top: -${convertPixelToRem(10)};
+  padding: 0 ${convertPixelToRem(20)};
+  padding-top: ${convertPixelToRem(40)};
+  padding-bottom: ${convertPixelToRem(48)};
+  border-top-left-radius: ${convertPixelToRem(10)};
+  border-top-right-radius: ${convertPixelToRem(10)};
+  background-color: ${palette.white};
+
+  & > h1 {
+    ${typography.t20b}
+    margin-bottom: ${convertPixelToRem(10)};
+  }
+  & > p {
+    ${typography.b14r}
+    margin-bottom: ${convertPixelToRem(32)};
+  }
+  & > button {
+    ${typography.b16sb}
+    width: 100%;
+    padding: ${convertPixelToRem(12)} 0;
+    text-align: center;
+    background-color: ${palette.orange600};
+    color: ${palette.white};
+    border: none;
+    border-radius: ${convertPixelToRem(6)};
+    cursor: pointer;
+  }
+`;
+
+const GroupInfoArea = styled.section`
+  & > ul {
+    margin-bottom: ${convertPixelToRem(32)};
+
+    & > li {
+      ${typography.b14r}
+      display: flex;
+
+      &:not(:last-child) {
+        margin-bottom: ${convertPixelToRem(10)};
+      }
+      & > h2 {
+        color: ${palette.grey500};
+        margin-right: ${convertPixelToRem(12)};
+      }
+      & > div {
+        color: ${palette.grey800};
+      }
+    }
+  }
+`;
+
+const Divider = styled.hr`
+  width: 100%;
+  height: ${convertPixelToRem(15)};
+  background-color: ${palette.grey100};
+  border: none;
+`;
+
+const PeopleArea = styled.section`
+  padding: ${convertPixelToRem(24)} ${convertPixelToRem(20)};
+`;
+
 export default SpecificStudy;
