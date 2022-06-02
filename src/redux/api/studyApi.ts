15dfc32b9e0fd6093932ccf939fb6fc1e3746174
Sehyeon Park <nesmile23@gmail.com>
2022-06-02 23:45:26 +0900

    chore: 그룹 중간장소 추천받기 api 추가

--- src/redux/api/studyApi.ts
+++ src/redux/api/studyApi.ts
@@ -1,3 +1,4 @@
+import { RecommendCafeData } from "@type/cafe";
 import {
   GroupInfo,
   GroupListResponse,
@@ -21,6 +22,7 @@ export const studyApi = createApi({
   }),
 
   endpoints: (builder) => ({
+    /** 스터디 검색 */
     getStudiesByOptions: builder.query<GroupListResponse, GroupsQueryOption>({
       query: (option) => {
         const queryParameter = getValidQueryParameter(option);
@@ -31,6 +33,7 @@ export const studyApi = createApi({
         };
       },
     }),
+    /** 스터디 생성하기 */
     createStudy: builder.query({
       query: (data) => ({
         url: `/api/groups`,
@@ -38,30 +41,42 @@ export const studyApi = createApi({
         body: data,
       }),
     }),
+    /** 가입된 스터디 목록 가져오기 */
     getMyStudyGroup: builder.query<GroupListResponse, null>({
       query: () => ({
         url: `/api/groups/my`,
         method: "GET",
       }),
     }),
+    /** 특정 스터디 정보 가져오기 */
     getSpecificStudy: builder.query<SpecificGroupResponse, string>({
       query: (groupId) => ({
         url: `/api/groups/${groupId}`,
         method: "GET",
       }),
     }),
+    /** 그룹 중간장소 추천받기 */
+    getRecommendationPlace: builder.query<RecommendCafeData, string>({
+      query: (groupId) => ({
+        url: `/api/groups/${groupId}/cafe-recommendation`,
+        method: "GET",
+      }),
+    }),
+    /** 스터디 가입 요청 */
     applyStudy: builder.mutation<ResponseData, string>({
       query: (groupId) => ({
         url: `/api/groups/${groupId}/apply`,
         method: "POST",
       }),
     }),
+    /** 스터디 탈퇴 */
     exitStudy: builder.mutation<ResponseData, GroupInfo["groupId"]>({
       query: (groupId) => ({
         url: `/api/groups/${groupId}/exit`,
         method: "POST",
       }),
     }),
+    /** 스터디 가입 승인 */
     approveMember: builder.mutation<
       ResponseData,
       { groupId: GroupInfo["groupId"]; memberId: MemberInfo["memberId"] }
@@ -71,6 +86,7 @@ export const studyApi = createApi({
         method: "POST",
       }),
     }),
+    /** 스터디 가입 거절 */
     denyMember: builder.mutation<
       ResponseData,
       { groupId: number; memberId: number }
@@ -92,4 +108,5 @@ export const {
   useExitStudyMutation,
   useApproveMemberMutation,
   useDenyMemberMutation,
+  useLazyGetRecommendationPlaceQuery,
 } = studyApi;
